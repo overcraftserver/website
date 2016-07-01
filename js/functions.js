7d2094e1a59221eb46a76aa5580814db34317e89
gaaalmeida <gaaalmeidasan@gmail.com>
2016-07-01 19:19:41 -0300

    Menu Scroll, e Font Toggle

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,16 @@
 $(function () {
-
   $(".mob_menuToggle").click(function () {
     $("#_mobile").toggleClass("ul_toggle");
   });
+});
 
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 200) {
+            $("#menu").removeClass("tofade");
+            $("#menu").addClass("toblack");
+        } else {
+            $("#menu").addClass("tofade");
+        }
+    });
 });
