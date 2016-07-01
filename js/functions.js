ed481853128fca40fc9a5302561a95c9cd283c87
gaaalmeida <gaaalmeidasan@gmail.com>
2016-07-01 20:05:45 -0300

    Grid e Section STAFF

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,17 @@
 $(function () {
   $(".mob_menuToggle").click(function () {
     $("#_mobile").toggleClass("ul_toggle");
+    $("#menu_mob").toggleClass("toblack");
   });
 });
 
 $(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 200) {
-            $("#menu").removeClass("tofade");
-            $("#menu").addClass("toblack");
-        } else {
-            $("#menu").addClass("tofade");
-        }
-    });
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 200) {
+      $(".dsk, #menu_mob").removeClass("tofade");
+      $(".dsk, #menu_mob").addClass("toblack");
+    } else {
+      $(".dsk, #menu_mob").addClass("tofade");
+    }
+  });
 });
