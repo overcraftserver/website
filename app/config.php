133a183e9771e3dcc0a6476c164c49e4d5be9f22
gaaalmeida <gaaalmeidasan@gmail.com>
2016-07-03 22:03:40 -0300

    Bug Fixes, PHP Var

--- app/config.php
+++ app/config.php
@@ -1,7 +1,12 @@
 <?php 
+//Variaveis
+$up = "up";
 
 // Configurações basicas do Site
 
 $titulo_do_site = "Overcraft Server";
+$version = "1.0.0";
+$prodStat = "Alpha";
+$update = $up + "0";
 
  ?>
